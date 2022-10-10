564235a3a670f8d7c086fa7a210e7fa204f40938
Victor Sousa <victorbrito001@gmail.com>
2022-10-10 14:30:55 -0300

    Estilos globais e cabecario da aplicacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { AppProps } from 'next/app'
 
-export default MyApp
+import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
+
+import { AppContainer, Header } from '../styles/pages/app'
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <AppContainer>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </AppContainer>
+  )
+}
